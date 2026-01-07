a9e959ba5edfd9dd0b6ca7461f8c70210f5dbbc5
bhashana99 <bhashanachamodya99@gmail.com>
2026-01-07 16:24:58 +0530

    Define the Custom User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    ROLE_CHOICES = (
+        ('USER', 'User'),
+        ('ADMIN', 'Admin'),
+    )
+    
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='USER')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    USERNAME_FIELD = 'email'
