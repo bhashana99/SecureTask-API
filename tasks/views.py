ac0c6c3baa33fdb8285f448e378d227f0e3d3179
bhashana99 <bhashanachamodya99@gmail.com>
2026-01-07 21:06:38 +0530

    task view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
+from .permissions import IsOwnerOrAdmin
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'ADMIN':
+            return Task.objects.all()
+        return Task.objects.filter(owner=user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
