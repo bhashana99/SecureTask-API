57150b035a9974230aae488eca090dd15f3eab4a
bhashana99 <bhashanachamodya99@gmail.com>
2026-01-07 16:42:12 +0530

    normal user create function

--- users/models.py
+++ users/models.py
@@ -1,17 +1,32 @@
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, role='USER'):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, role=role)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     ROLE_CHOICES = (
         ('USER', 'User'),
         ('ADMIN', 'Admin'),
     )
-    
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='USER')
+
+    role = models.CharField(
+        max_length=10, choices=ROLE_CHOICES, default='USER')
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     USERNAME_FIELD = 'email'
