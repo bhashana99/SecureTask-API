82fde50b6cc82d106c38bb578e28b9682fc3fa3a
bhashana99 <bhashanachamodya99@gmail.com>
2026-01-07 20:51:50 +0530

    create permission file for give access

---
+++ tasks/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.role == 'ADMIN':
+            return True
+        return obj.owner == request.user
